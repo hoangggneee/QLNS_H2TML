17177adf68d627b3497468f08cc360564ee50468
hoangggneee <hoangv270301@gmail.com>
2023-03-29 13:57:54 +0700

    update template bị lỗi
    FIX BUG

--- src/main/java/h2tml/qlns/repository/ProjectRepository.java
+++ src/main/java/h2tml/qlns/repository/ProjectRepository.java
@@ -1,6 +1,10 @@
 package h2tml.qlns.repository;
 
+import h2tml.qlns.model.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ProjectRepository {
+@Repository
+public interface ProjectRepository extends JpaRepository <Project, Long> {
 
 }
